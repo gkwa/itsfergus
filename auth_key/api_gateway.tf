041be9d1d34a5a9e86ef655997829d85f20668d2
Taylor Monacelli <taylormonacelli@gmail.com>
2024-12-04 13:49:06 -0800

    st

--- auth_key/api_gateway.tf
+++ auth_key/api_gateway.tf
@@ -1,12 +1,24 @@
-resource "aws_apigatewayv2_api" "lambda_api" {
-  name          = "lambda-api"
-  protocol_type = "HTTP"
+resource "aws_api_gateway_rest_api" "lambda_api" {
+  name = "lambda-api"
 }
 
-resource "aws_apigatewayv2_stage" "lambda_stage" {
-  api_id      = aws_apigatewayv2_api.lambda_api.id
-  name        = "$default"
-  auto_deploy = true
+resource "aws_api_gateway_deployment" "lambda_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_method.lambda_method
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "lambda_stage" {
+  deployment_id = aws_api_gateway_deployment.lambda_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
+  stage_name    = "prod"
 }
 
 resource "aws_iam_role" "api_gateway_execution_role" {
@@ -42,20 +54,29 @@ resource "aws_iam_role_policy" "api_gateway_lambda_policy" {
   })
 }
 
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id             = aws_apigatewayv2_api.lambda_api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = var.aws_lambda_function_arn
-  integration_method = "POST"
-  credentials_arn    = aws_iam_role.api_gateway_execution_role.arn
+resource "aws_api_gateway_resource" "lambda_resource" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  parent_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
+  path_part   = "{proxy+}"
 }
 
-resource "aws_apigatewayv2_route" "lambda_route" {
-  api_id             = aws_apigatewayv2_api.lambda_api.id
-  route_key          = "ANY /"
-  target             = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-  authorization_type = "NONE"
-  api_key_required   = true
+resource "aws_api_gateway_method" "lambda_method" {
+  rest_api_id      = aws_api_gateway_rest_api.lambda_api.id
+  resource_id      = aws_api_gateway_resource.lambda_resource.id
+  http_method      = "ANY"
+  authorization    = "NONE"
+  api_key_required = true
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.lambda_resource.id
+  http_method = aws_api_gateway_method.lambda_method.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = var.aws_lambda_function_arn
+  credentials             = aws_iam_role.api_gateway_execution_role.arn
 }
 
 resource "aws_api_gateway_api_key" "lambda_key" {
@@ -66,8 +87,8 @@ resource "aws_api_gateway_usage_plan" "lambda_usage_plan" {
   name = "lambda-usage-plan"
 
   api_stages {
-    api_id = aws_apigatewayv2_api.lambda_api.id
-    stage  = aws_apigatewayv2_stage.lambda_stage.name
+    api_id = aws_api_gateway_rest_api.lambda_api.id
+    stage  = aws_api_gateway_stage.lambda_stage.stage_name
   }
 }
 
--- auth_key/outputs.tf
+++ auth_key/outputs.tf
@@ -1,5 +1,5 @@
 output "api_gateway_url" {
-  value = aws_apigatewayv2_stage.lambda_stage.invoke_url
+  value = "${aws_api_gateway_stage.lambda_stage.invoke_url}/{proxy}"
 }
 
 output "api_key" {
@@ -8,5 +8,5 @@ output "api_key" {
 }
 
 output "api_gateway_execution_arn" {
-  value = aws_apigatewayv2_api.lambda_api.execution_arn
+  value = aws_api_gateway_rest_api.lambda_api.execution_arn
 }
