091a7e8e8fe51c65d5d8bdee1664e93b3ae21045
Taylor Monacelli <taylormonacelli@gmail.com>
2024-12-04 11:54:51 -0800

    merge with conditional

---
+++ auth_iam/iam.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_role" "api_invocation_role" {
+ name = "api_invocation_role"
+
+ assume_role_policy = jsonencode({
+   Version = "2012-10-17"
+   Statement = [
+     {
+       Action = "sts:AssumeRole"
+       Effect = "Allow"
+       Principal = {
+         AWS = data.aws_caller_identity.current.arn
+       }
+     }
+   ]
+ })
+}
+
+resource "aws_iam_role_policy" "api_invocation_policy" {
+ name = "api_invocation_policy"
+ role = aws_iam_role.api_invocation_role.id
+
+ policy = jsonencode({
+   Version = "2012-10-17"
+   Statement = [
+     {
+       Effect = "Allow"
+       Action = [
+         "execute-api:Invoke",
+         "execute-api:ManageConnections"
+       ]
+       Resource = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*"
+     }
+   ]
+ })
+}
+
--- variables.tf
+++ variables.tf
@@ -1,17 +1,10 @@
-variable "aws_region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-  default     = "ca-central-1"
-}
-
-variable "ecr_repository_name" {
-  description = "Name of the ECR repository"
-  type        = string
-  default     = "lambda-docker-repo"
-}
-
-variable "lambda_function_name" {
-  description = "Name of the Lambda function"
-  type        = string
-  default     = "docker-lambda-function"
+variable "auth_type" {
+ description = "Authentication type (iam or key)"
+ type        = string
+ default     = "iam"
+ 
+ validation {
+   condition     = contains(["iam", "key"], var.auth_type)
+   error_message = "auth_type must be either 'iam' or 'key'"
+ }
 }
