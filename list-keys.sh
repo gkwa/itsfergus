c400ff245186092df2c0a89806fd7920f3af0955
Taylor Monacelli <taylormonacelli@gmail.com>
2024-12-09 10:11:19 -0800

    stuff

--- list-keys.sh
+++ list-keys.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-
-# Usage: ./list-keys.sh 10m/20h (default: 12h)
 duration=${1:-12h}
 num=${duration%[hm]}
 unit=${duration#${num}}
 
 if [[ $unit == "m" ]]; then
-    hours=$(bc <<<"scale=2; $num/60")
+   mins="-${num} minutes"
 else
-    hours=$num
+   mins="-${num} hours"
 fi
 
 echo "KMS Keys found in logs since ${duration} ago:"
 
 PAGER=cat aws cloudtrail lookup-events \
-    --region ca-central-1 \
-    --start-time "$(date -u -d "${hours} hours ago" '+%Y-%m-%dT%H:%M:%SZ')" \
-    --query 'Events[?EventSource==`kms.amazonaws.com`].Resources[*].ResourceName' \
-    --output text | tr '\t' '\n' | sort -u
+ --region ca-central-1 \
+ --start-time "$(date -u -d "${mins}" '+%Y-%m-%dT%H:%M:%SZ')" \
+ --query 'Events[?EventSource==`kms.amazonaws.com`].Resources[*].ResourceName' \
+ --output text | tr '\t' '\n' | sort -u
